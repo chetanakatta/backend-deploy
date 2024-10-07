316ee0bb7594034590082bd85793de79ef37e682
chetana <chetana@gmail.com>
2024-10-07 13:49:13 -0400

    testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ resource "null_resource" "backend" {
     provisioner "remote-exec" {
         inline = [
             "chmod +x /tmp/${var.common_tags.Component}.sh",   #execution permission
-            "sudo sh /tmp/${var.common_tags.Component}.sh ${var.common_tags.Component} ${var.environment}"     #root access
+            "sudo sh /tmp/${var.common_tags.Component}.sh ${var.common_tags.Component} ${var.environment} ${var.app_version}"     #root access
         ]
     }
 }
